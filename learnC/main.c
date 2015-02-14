80f788958e4cea141a812b22a8c0b1c56f6c5ae3
sisi <lusisi00@gmail.com>
2015-02-13 20:46:10 -0500

    add learnC

---
+++ learnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learnC
+//
+//  Created by lusisi on 2/13/15.
+//  Copyright (c) 2015 lusisi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
